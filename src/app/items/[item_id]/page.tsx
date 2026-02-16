f5eed8f1f6eb1ff28e3fc64150cf52e99756c0a3
karthik-provus <karthik@provus.ai>
2026-02-16 16:28:16 +0530

    useRouter vs redirect

--- src/app/items/[item_id]/page.tsx
+++ src/app/items/[item_id]/page.tsx
@@ -1,5 +1,7 @@
+"use client";
 import { use } from "react";
 import LinkItems from "../page";
+import { useRouter } from "next/navigation";
 
 export default function Item_Id({params, searchParams}:{
     params: Promise<{item_id: string}>,
@@ -7,10 +9,14 @@ export default function Item_Id({params, searchParams}:{
 }) {
     const {item_id} = use(params);
     const {q} = use(searchParams);
+
+    const router = useRouter();
   return (
     <div>
         This is item with id: {item_id} and search param: {q}
         <LinkItems />
+
+        <button onClick={()=> router.push("/about")}>Go to About Page</button>
     </div>
   )
 }
--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,3 +1,5 @@
+import { redirect } from "next/navigation"
+
 export default async function ProductPage({
   params,
 }: {
@@ -6,6 +8,11 @@ export default async function ProductPage({
   params: Promise<{ productId: string }>
 }) {
   const { productId } = await params
-
+  if(Number.parseInt(productId)>1000){
+    redirect('/product')
+    // redirect stops the execution right here.
+    // this is a server side method
+    // redirect method throws an error which nextjs detects
+  }
   return <h1>Product ID: {productId}</h1>
 }
